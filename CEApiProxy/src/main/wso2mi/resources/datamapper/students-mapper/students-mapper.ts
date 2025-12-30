22bea582a6ca0bc242cf8d19fc380c86fcace9d2
Chamuditha Nawarathna <150885555+ChamudithaNawarathna@users.noreply.github.com>
2025-12-30 09:03:33 +0530

    Implement CeProxyAPI with endpoints for students and projects

---
+++ CEApiProxy/src/main/wso2mi/resources/datamapper/students-mapper/students-mapper.ts
@@ -0,0 +1,146 @@
+import * as dmUtils from "./dm-utils";
+declare var DM_PROPERTIES: any;
+
+/*
+* title : "root",
+* inputType : "JSON",
+*/
+interface Root {
+E02: {
+batch: string
+url: string
+count: number
+}
+e02a: {
+batch: string
+url: string
+count: number
+}
+E04: {
+batch: string
+url: string
+count: number
+}
+E05: {
+batch: string
+url: string
+count: number
+}
+E06: {
+batch: string
+url: string
+count: number
+}
+E07: {
+batch: string
+url: string
+count: number
+}
+E08: {
+batch: string
+url: string
+count: number
+}
+E10: {
+batch: string
+url: string
+count: number
+}
+E11: {
+batch: string
+url: string
+count: number
+}
+E12: {
+batch: string
+url: string
+count: number
+}
+E13: {
+batch: string
+url: string
+count: number
+}
+E14: {
+batch: string
+url: string
+count: number
+}
+E15: {
+batch: string
+url: string
+count: number
+}
+E16: {
+batch: string
+url: string
+count: number
+}
+E17: {
+batch: string
+url: string
+count: number
+}
+E18: {
+batch: string
+url: string
+count: number
+}
+E19: {
+batch: string
+url: string
+count: number
+}
+E20: {
+batch: string
+url: string
+count: number
+}
+E21: {
+batch: string
+url: string
+count: number
+}
+E22: {
+batch: string
+url: string
+count: number
+}
+E23: {
+batch: string
+url: string
+count: number
+}
+E96: {
+batch: string
+url: string
+count: number
+}
+E97: {
+batch: string
+url: string
+count: number
+}
+E99: {
+batch: string
+url: string
+count: number
+}
+}
+
+
+/**
+* outputType:unknown
+*/
+interface OutputRoot {
+}
+
+
+/**
+ * functionName : map_S_root_S_Root
+ * inputVariable : inputroot
+*/
+export function mapFunction(input: Root): OutputRoot {
+    return {}
+}
+
